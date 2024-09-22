838314d7e92ceb218c6dde6c03b9e92b9a505aa4
Nikhila Sornapudi <nikhila2410@gmail.com>
2024-09-22 17:43:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Add this line to enable CORS for all routes
+CORS(app)
+
+# Global variable to store the processed data
+processed_data = {}
 
 @app.route('/bfhl', methods=['POST'])
 def process_data():
+    global processed_data
     data = request.json.get('data', [])
     numbers = [x for x in data if x.isdigit()]
     alphabets = [x for x in data if x.isalpha()]
     highest_alphabet = max(alphabets, key=str.lower) if alphabets else ""
-    
-    response = {
+
+    processed_data = {
         "is_success": True,
         "user_id": "john_doe_17091999",
         "email": "john@xyz.com",
@@ -21,11 +25,11 @@ def process_data():
         "alphabets": alphabets,
         "highest_alphabet": [highest_alphabet] if highest_alphabet else []
     }
-    return jsonify(response)
+    return jsonify(processed_data)
 
 @app.route('/bfhl', methods=['GET'])
 def get_operation_code():
-    return jsonify({"operation_code": 1})
+    return jsonify({"operation_code": 1, "data": processed_data})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 4000))
